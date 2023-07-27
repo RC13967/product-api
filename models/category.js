272f9cd2e4d744665d1fff754f3daa95f300b764
RC13976 <ranjithch137@gmail.com>
2023-07-27 17:40:25 +0530

    unique values

--- models/category.js
+++ models/category.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 const categorySchema = new mongoose.Schema({
-    category_name: { type: String, required: true },
-    category_description: { type: String, required: true },
+    category_name: { type: String, unique:true, required: true,
+        index: { unique: true, collation: { locale: 'en', strength: 2 } } },
+    category_description: { type: String, unique:true, required: true,
+        index: { unique: true, collation: { locale: 'en', strength: 2 } } },
     is_active: { type: Boolean, required: true },
     created_at: { type: Date, required: true },
     updated_at: { type: Date, required: true },
--- models/product.js
+++ models/product.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-    product_name: { type: String, required: true },
-    product_description: { type: String, required: true },
+    product_name: { type: String, unique:true, required: true,
+        index: { unique: true, collation: { locale: 'en', strength: 2 } } },
+    product_description: { type: String, unique:true, required: true,
+        index: { unique: true, collation: { locale: 'en', strength: 2 } } },
     category_id: {type: String, required: true},
-    product_image: {type: String, required: false},
+    product_image: {type: String, unique:true, required: true},
     product_quantity: {type: Number, required: true},
     is_active: { type: Boolean, required: true },
     created_at: { type: Date, required: true },
